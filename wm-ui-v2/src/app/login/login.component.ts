43d904f9c9a9e254980dd29832a02cf938df219d
Liane Stevenson <knightowlcrm@gmail.com>
2019-03-02 22:28:17 +0000

    #12 [Bug] - Continue Button Wrong Link
    User should continue to the Dashboard and be logged in

--- wm-ui-v2/src/app/login/login.component.ts
+++ wm-ui-v2/src/app/login/login.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from "angularx-social-login";
 import { GoogleLoginProvider } from "angularx-social-login";
-import { SocialUser } from "angularx-social-login";
 import { LoginServiceService } from '../services/login-service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -14,7 +14,7 @@ export class LoginComponent implements OnInit {
   user: any;
   loggedIn: boolean;
 
-  constructor(private authService: AuthService, private _login: LoginServiceService) { }
+  constructor(private authService: AuthService, private _login: LoginServiceService, private router: Router) { }
 
   ngOnInit() {
     this.authService.authState.subscribe((user) => {
@@ -30,6 +30,10 @@ export class LoginComponent implements OnInit {
       this.user = user;
       localStorage.setItem('user', JSON.stringify(this.user));
     });
+  }
+ 
+  continue(): void {
+    this.router.navigateByUrl("/dashboard");
   }
 
   signOut(): void {
